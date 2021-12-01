ed9fdbd1c217d59ffde3d743cb598d847e0d0792
phihoang1107 <92515483+phihoang1107@users.noreply.github.com>
2021-12-01 16:55:37 +0700

    show password

--- src/App.js
+++ src/App.js
@@ -1,47 +1,65 @@
 import { useState } from 'react'
-import Job from './name/Job';
+// import Job from './name/Job';
 
 
 
 
 
 function App() {
-  
-  
-
-   
-    const [job, setJob] = useState('')
-    const [jobs, setJobs] = useState([])
-    const handleSubmit = () =>{
-        setJobs(prev => {
-          const newJobs = [...prev, job]
-          const jsonJobs = JSON.stringify(newJobs)
-          localStorage.setItem('jobs', newJobs)
-          console.log('aloalo')
-          return newJobs
-        })
-        setJob('')
-        
-    }
+    const [toggle, setToggle] = useState(true)
+    const [inp, setInp] = useState('true')
+    const [togglepassword, setTogglepassword] = useState('true')
+    const [toggleconfirm, setToggleconfirm] = useState('true')
     
+        
+   
+
+    const changeValue =() => {
+
+    }
+    const handleClick = () =>{
+        setTogglepassword(!togglepassword)
+        setToggleconfirm(!toggleconfirm)
+    }
+
     return (
         <div style={{ padding: 30}}>
-        <input type="text" onChange={(e)=>setJob(e.target.value)} value={job}/>
-         
-         
-        
-         
-         <button onClick={handleSubmit}>ADD</button> 
-
-
-         <ul>
-             {jobs.map((job, index) =>  (
-                 <li key={index}>{job}</li>
-
-             ))}
-         </ul>   
+            <div>Alo</div>
+            {toggle ?  <input type="text" value={inp} onChange={(e)=>{setInp(e.target.value)}}/> : 
+                <div>
+                    <div>
+                        <input id="firstName" type="text" name="firstname" placeholder="First Name" />
+                    </div>
+                    <div>
+                        <input id="firstName" type="text" name="firstname" placeholder="First Name" />
+                    </div>
+                    <div>
+                        <input id="email" type="email" name="email" placeholder="Email Address" />
+                    </div>
+                    <div> 
+                        <input id="phoneNumber" type="tel" name="phone" placeholder="Phone Number" />
+                    </div>
+                    <div>
+                        <input id="password" type={togglepassword?"type":"password"} name="password" placeholder="Password" />
+                        <button onClick={() =>setTogglepassword(!togglepassword)}>Show Password</button>
+                       
+                    </div>
+                    <div>
+                        <input id="passwordConfirmation" type="text" name="passwordConfirmation" placeholder="Confirm Password"/>
+                        <button>Show ConfirmPassword</button>
+                    </div>
+                </div>
+            }
+            
+            <button onClick={()=>{
+                setToggle(!toggle)
+            
+            }}>{toggle?'Create Account':'Close'}</button> 
         </div>
+    
     )
-}
 
+}
+   
 export default App;
+
